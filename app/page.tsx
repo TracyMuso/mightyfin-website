248a8da6283049291a84c6c5a9edad17ea362359
TracyMuso <musongoletracy@gmail.com>
2025-04-15 10:26:16 +0200

    feat(landing-page): add why-us section on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero";
 import Products from "@/components/Products";
+import Whyus from "@/components/Whyus";
 export default function Home() {
   return (
     <div>
       <Hero />
       <Products />
+      <Whyus />
     </div>
   );
 }
