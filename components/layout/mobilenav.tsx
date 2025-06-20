7811f0426c29d86398d49978c687143f87623886
Tracy Musongole <musongoletracy@gmail.com>
2025-06-20 13:21:40 +0200

    Update mobilenav.tsx
    added new login ink

--- components/layout/mobilenav.tsx
+++ components/layout/mobilenav.tsx
@@ -85,7 +85,7 @@ export default function MobileNav() {
             </a>
           ))}
           <Link
-            href={"auth/signin"}
+            href={"https://app.mightyfinance.co.zm/login"}
             className="bg-purple-800 hover:bg-purple-700 px-5 py-2 rounded-lg text-white text-sm lg:text-[16px] font-semibold"
           >
             Log in
